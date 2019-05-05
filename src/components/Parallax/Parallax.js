334c55061c85a4cf3c84d03eef3079ace1833686
Sophie van Duivenboden <>
2019-05-05 17:53:56 +0200

    add the paralax effect

--- src/components/Parallax/Parallax.js
+++ src/components/Parallax/Parallax.js
@@ -3,20 +3,16 @@ import './Parallax.scss';
 
 class Parallax extends React.Component {
   state = {
-    containerHeight: 800,
+    containerHeight: window.innerHeight,
     containerWidth: 0,
     x: 0,
     y: 0
   };
 
   onMouseMove = (e) => {
-    // console.log(position, e.nativeEvent.offsetX, e.screenX);
     let calculateMiddleY = ((this.state.containerHeight/2) - e.pageY)*-1;
     let calculateMiddleX = ((this.state.containerWidth/2) - e.pageX)*-1;
-
-
     this.setState({ x: calculateMiddleX, y: calculateMiddleY });
-
   }
 
   componentDidMount = () => {
@@ -44,14 +40,35 @@ class Parallax extends React.Component {
       <h1>Mouse coordinates: { x } { y }</h1>
         <div className="container">
           <div className="block_container">
+            <div className="block fifth">
+              <div className="parallax_block_fifth" style={{
+                left: (x*0.05),
+                top: (y*0.05),
+              }}></div>
+            </div>
+            <div className="block fourth">
+              <div className="parallax_block_fourth" style={{
+                left: (x*0.2),
+                top: (y*0.2),
+              }}></div>
+            </div>
             <div className="block third">
-              <div className="parallax_block_third"></div>
+              <div className="parallax_block_third" style={{
+                left: (x*0.35),
+                top: (y*0.35),
+              }}></div>
             </div>
             <div className="block second">
-              <div className="parallax_block_second"></div>
+              <div className="parallax_block_second" style={{
+                left: (x*0.5),
+                top: (y*0.5),
+              }}></div>
             </div>
             <div className="block first">
-            <div className="parallax_block_first"></div>
+              <div className="parallax_block_first" style={{
+                left: (x*0.65),
+                top: (y*0.65),
+              }}></div>
             </div>
           </div>
         </div>
